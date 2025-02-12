918545fa1997a8f88de66fad7618aeff8d010cad
Luofan Chen <clfbbn@gmail.com>
2025-02-12 09:07:52 +0000

    qcom: display-drivers: dsa_panel: Send notify event on aod mode change
    The lxs_ts driver will need to perform tasks on entering / leaving AOD
    mode so that dt2w will work.
    
    Change-Id: I225099bd9e2a907add74b46ed2713f4029d0c79d

--- qcom/opensource/display-drivers/msm/dsi/dsi_panel.c
+++ qcom/opensource/display-drivers/msm/dsi/dsi_panel.c
@@ -10,6 +10,7 @@
 #include <linux/of_gpio.h>
 #include <linux/pwm.h>
 #include <video/mipi_display.h>
+#include <linux/drm_notify.h>
 
 #include "dsi_panel.h"
 #include "dsi_ctrl_hw.h"
@@ -4621,6 +4622,7 @@ int dsi_panel_set_lp1(struct dsi_panel *panel)
 {
 	int rc = 0;
 	int bl_lvl;
+	struct drm_ext_event event;
 
 	if (!panel) {
 		DSI_ERR("invalid params\n");
@@ -4654,6 +4656,10 @@ int dsi_panel_set_lp1(struct dsi_panel *panel)
 
 	bl_lvl = dsi_panel_get_backlight(panel);
 	dsi_panel_set_backlight(panel, bl_lvl);
+
+	event.data = &(panel->spec_pdata->aod_mode);
+	drm_notifier_call_chain(DRM_EXT_EVENT_AOD_CHANGE, &event);
+
 #else
 	if (rc)
 		DSI_ERR("[%s] failed to send DSI_CMD_SET_LP1 cmd, rc=%d\n",
@@ -4689,6 +4695,7 @@ exit:
 int dsi_panel_set_nolp(struct dsi_panel *panel)
 {
 	int rc = 0;
+	struct drm_ext_event event;
 
 	if (!panel) {
 		DSI_ERR("invalid params\n");
@@ -4716,6 +4723,9 @@ int dsi_panel_set_nolp(struct dsi_panel *panel)
 		panel->spec_pdata->aod_mode = 0;
 		pr_notice("%s: set AOD mode OFF\n", __func__);
 	}
+
+	event.data = &(panel->spec_pdata->aod_mode);
+	drm_notifier_call_chain(DRM_EXT_EVENT_AOD_CHANGE, &event);
 #else
 	if (rc)
 		DSI_ERR("[%s] failed to send DSI_CMD_SET_NOLP cmd, rc=%d\n",
